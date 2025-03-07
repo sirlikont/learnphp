408ac4f4b71033f2c67e76c8188b0956ad54033d
Sirli Kont <sirlikont@gmail.com>
2025-03-07 09:42:58 +0200

    algteadmised

---
+++ index.php
@@ -0,0 +1,43 @@
+<?php
+//üherealine kommentaar
+
+/* 
+mitmrealine
+mitmerealine
+kommentaa
+*/ 
+
+echo('Hello world!');
+echo(Hello world);
+
+$var = 'Muutuja';
+$var = 4; //int
+$var = 3.4 //float
+$var = true //bool
+$var = null; //null
+$var = ['hello', 2, 4, true]; //array
+$var = newStdClass(); //object
+foreach($var as $key => $value){
+    var_dump(var$);
+}
+$var = [
+    'name' => 'Kai',
+    'age' => 32,
+    'has cat' => true,
+    43,
+    'absd',
+    true
+
+];
+var_dump($var);
+var_dump($var[0]);
+var_dump($var['name']);
+
+function hello($name) {
+    echo 'hello' . $name
+};
+hello('Kaspar');
+$name = 'Martin';
+echo "dont be an a** \n $name"; //vaja kasutada topelt jutumärke
+
+?>
