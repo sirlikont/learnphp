922275e6adb84c9d07af2743169cc09c8dc56b5e
Sirli Kont <sirlikont@gmail.com>
2025-03-07 11:15:58 +0200

    trait

--- index.php
+++ index.php
@@ -1,43 +1,37 @@
 <?php
-//üherealine kommentaar
 
-/* 
-mitmrealine
-mitmerealine
-kommentaa
-*/ 
-
-echo('Hello world!');
-echo(Hello world);
-
-$var = 'Muutuja';
-$var = 4; //int
-$var = 3.4 //float
-$var = true //bool
-$var = null; //null
-$var = ['hello', 2, 4, true]; //array
-$var = newStdClass(); //object
-foreach($var as $key => $value){
-    var_dump(var$);
+class Cat {
+    use HasWeight;
 }
-$var = [
-    'name' => 'Kai',
-    'age' => 32,
-    'has cat' => true,
-    43,
-    'absd',
-    true
 
-];
-var_dump($var);
-var_dump($var[0]);
-var_dump($var['name']);
+class Gun {
+    use HasWeight, HasAmmo;
+}
 
-function hello($name) {
-    echo 'hello' . $name
-};
-hello('Kaspar');
-$name = 'Martin';
-echo "dont be an a** \n $name"; //vaja kasutada topelt jutumärke
+class CrossBow {
+    use HasAmmo;
+}
 
+trait HasAmmo{
+    public $ammo;
+}
+
+trait HasWeight {
+    private $weight;
+
+    public function getWeight(){
+        return $this->weight;
+    }
+    public function setWeight($weight){
+        $this->weight = $weight;
+        if($weight < 0){
+            $this->weight = 0;
+        };
+    }
+}
+
+$cat = new Cat();
+$gun = new Gun();
+$crossbow = new CrossBow;
+var_dump($cat, $gun, $crossbow);
 ?>
